6925099a1e78cdec5134ed237d9b7bfbb2979e29
Brian Merchant <bzm3r@proton.me>
2024-09-02 18:54:06 -0700

    the $prop rune for an optional value

--- src/tutorial/Nested.svelte
+++ src/tutorial/Nested.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	interface NestedProps {
-		answer: number;
+		answer?: number | string;
 	}
-	let { answer }: NestedProps = $props();
+	let { answer = 'a mystery' }: NestedProps = $props();
 </script>
 
 <p>The answer is {answer}</p>
