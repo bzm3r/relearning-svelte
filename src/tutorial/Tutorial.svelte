3b96e58f5c12ed2aaeccadd1378a0139f6018e19
Brian Merchant <bzm3r@proton.me>
2024-09-02 17:02:17 -0700

    the $derived rune

--- src/tutorial/Tutorial.svelte
+++ src/tutorial/Tutorial.svelte
@@ -4,9 +4,13 @@
 	function increment() {
 		count++;
 	}
+
+	let doubled = $derived(count * 2);
 </script>
 
 <button onclick={increment}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
+
+<p>{count} doubled is {doubled}</p>
