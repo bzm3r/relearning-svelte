d9f7aa3346045e6eb4fd9c76147d686b82a01913
Brian Merchant <bzm3r@proton.me>
2024-09-02 19:30:02 -0700

    svelte addition to HTML: each block

--- src/tutorial/Tutorial.svelte
+++ src/tutorial/Tutorial.svelte
@@ -1,20 +1,45 @@
-<script>
-	let count = $state(0);
-
-	function increment() {
-		count += 1;
-	}
+<script lang="ts">
+	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+	let selected = $state(colors[0]);
 </script>
 
-<button onclick={increment}>
-	Clicked {count}
-	{count === 1 ? 'time' : 'times'}
-</button>
+<h1 style="color: {selected}">Pick a colour</h1>
 
-{#if count > 10}
-	<p>{count} is greater than 10</p>
-{:else if count == 0}
-	<p>press the button to get started!</p>
-{:else}
-	<p>you have to press the button {10 - count} more times!</p>
-{/if}
+<div>
+	{#each colors as color, i}
+		<button
+			aria-current={selected === color}
+			aria-label={color}
+			style="background: {color}"
+			onclick={() => (selected = color)}>{i + 1}</button
+		>
+	{/each}
+</div>
+
+<style>
+	h1 {
+		transition: color 0.2s;
+	}
+
+	div {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		grid-gap: 5px;
+		max-width: 400px;
+	}
+
+	button {
+		aspect-ratio: 1;
+		border-radius: 50%;
+		background: var(--color, #fff);
+		transform: translate(-2px, -2px);
+		filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
+		transition: all 0.1s;
+	}
+
+	button[aria-current='true'] {
+		transform: none;
+		filter: none;
+		box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
+	}
+</style>
