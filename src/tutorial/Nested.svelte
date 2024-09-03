903267f787292f59339db3c3d4359e308999c5da
Brian Merchant <bzm3r@proton.me>
2024-09-02 18:50:32 -0700

    adding some typescript

--- src/tutorial/Nested.svelte
+++ src/tutorial/Nested.svelte
@@ -1,5 +1,8 @@
-<script>
-	let { answer } = $props();
+<script lang="ts">
+	interface NestedProps {
+		answer: number;
+	}
+	let { answer }: NestedProps = $props();
 </script>
 
 <p>The answer is {answer}</p>
--- src/tutorial/Tutorial.svelte
+++ src/tutorial/Tutorial.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Nested from './Nested.svelte';
 </script>
 
